334351bd236bd19584ec1ba5cdae4634bb94b19c
Aram Baghdasarian <aram99551@gmail.com>
2021-10-11 14:50:52 +0400

    Add project files.

---
+++ User/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace User
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User();
+            User user2 = new User();
+            User user3 = new User();
+            User user4 = new User();
+            User user5 = new User();
+            Console.WriteLine(user1.Id);
+            Console.WriteLine(user3.Id);
+            Console.WriteLine(user5.Id);
+
+            User.Display();
+        }
+    }
+}
---
+++ User/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User
+{
+    class User
+    {
+        private static int counter = 0;
+        public int Id { get; set; }
+        static User()
+        {
+            Console.WriteLine("First user is created");
+        }
+        public User()
+        {
+            Id = ++counter;
+        }
+       
+        public static void Display()
+        {
+            Console.WriteLine($"{counter}");
+        }
+    }
+}
